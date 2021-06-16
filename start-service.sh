7507403d50d2f60c426935443dafabb1e344ae8b
Luis Gabriel Avella <luis.avellap@gmail.com>
2021-06-15 22:20:07 -0500

    Modified script send request server

---
+++ start-service.sh
@@ -0,0 +1,1 @@
+nohup python3 server_init.py >> output.txt &
--- test_client.py
+++ test_client.py
@@ -1,8 +1,9 @@
 import socket
 
+# UDP_IP_ADDRESS = "157.245.249.100"
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
-Message = "Hello, Server"
+Message = "C-02-053,2021-06-12,1018"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
