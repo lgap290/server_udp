9e7a4625769d02d670d8956ef21ac6f4f866db3b
Luis Gabriel Avella <luis.avellap@gmail.com>
2021-06-15 22:20:02 -0500

    Modified script send request server

--- server_init.py
+++ server_init.py
@@ -1,5 +1,6 @@
 import logging
 import socket
+import requests
 
 log = logging.getLogger('udp_server')
 
@@ -18,9 +19,19 @@ def udp_server(host='127.0.0.1', port=1234):
 FORMAT_CONS = '%(asctime)s %(name)-12s %(levelname)8s\t%(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT_CONS)
 
+# UDP_IP_ADDRESS = "157.245.249.100"
+# UDP_PORT_NO = 6789
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
 
 for data in udp_server(UDP_IP_ADDRESS, UDP_PORT_NO):
-    log.debug("%r" % (data,))
+    log.debug("Message received: %r" % (data,))
+    data_antena = data.split(',')
+    response = requests.post("http://localhost:8000/api/consume", data = {
+        "id":data_antena[0],
+        "date":data_antena[1],
+        "consume":data_antena[2],
+    })
+    log.debug("Message save: %r" % (response,))
+    
 
